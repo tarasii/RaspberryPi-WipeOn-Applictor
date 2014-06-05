c3c0f18dff9b33018d49a23caa7cee086ad27dee
tarasii <tarasii@gmail.com>
2014-06-05 12:39:21 +0300

    Create printonkeyborard.py

---
+++ printonkeyborard.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+#
+import sys
+import tty, termios
+import shutil
+import setbarcode
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+while 1==1:
+    ch = getch()
+    if ch == 'q':
+        break;
+
+    #print ch
+    setbarcode.barcodefromconfig()
+    shutil.copy('/home/pi/barcode.bas', '/home/pi/dev.lp0')
