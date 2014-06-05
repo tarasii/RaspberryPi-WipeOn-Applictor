322811856c0a32b07a03fe61d5ad9374b33e52c7
tarasii <tarasii@gmail.com>
2014-06-05 17:53:00 +0300

    Update adm.py
    add enterprise code to barcode config

--- adm.py
+++ adm.py
@@ -31,7 +31,7 @@ def chckc(cnf, name, default = ""):
 
 def fromfile():
     fname = '/home/pi/printer.config';
-    tp_def = (False,"00","000000000000","white","C0","Editing...")
+    tp_def = (False,"00","000000000000","white","C0","XXX","Editing...")
     if not os.path.isfile(fname):
         return tp_def
 
@@ -39,11 +39,12 @@ def fromfile():
     config = ConfigParser.RawConfigParser()
     config.readfp(fp)
     tp_res = (chckc(config,'manual',tp_def[0]),
-        chckc(config,'line',    tp_def[1]),
-        chckc(config,'barcode', tp_def[2]),
-        chckc(config,'color',   tp_def[3]),
-        chckc(config,'category',tp_def[4]),
-        tp_def[5])
+        chckc(config,'line',       tp_def[1]),
+        chckc(config,'barcode',    tp_def[2]),
+        chckc(config,'color',      tp_def[3]),
+        chckc(config,'category',   tp_def[4]),
+        chckc(config,'enterprise', tp_def[5]),
+        tp_def[6])
     fp.close()
     return tp_res
 
@@ -59,11 +60,12 @@ def fromform():
 
 def savetofile():
     config.add_section('main')
-    config.set('main', 'manual',   tp[0])
-    config.set('main', 'line',     tp[1])
-    config.set('main', 'barcode',  tp[2])
-    config.set('main', 'color',    tp[3])
-    config.set('main', 'category', tp[4])
+    config.set('main', 'manual',     tp[0])
+    config.set('main', 'line',       tp[1])
+    config.set('main', 'barcode',    tp[2])
+    config.set('main', 'color',      tp[3])
+    config.set('main', 'category',   tp[4])
+    config.set('main', 'enterprise', tp[5])
     fp = open('/home/pi/printer.config','wb')
     config.write(fp);
     
@@ -87,6 +89,6 @@ ft = open('/var/www/cgi-bin/adm.html')
 str = ft.read()
 ft.close()
 
-z = (buf,) + tp[1:3] + cl_d[tp[3]] + ct_d[tp[4]] + (tp[5],)
+z = (buf,) + tp[1:3] + cl_d[tp[3]] + ct_d[tp[4]] + (tp[5], tp[6])
 
 print str % z
