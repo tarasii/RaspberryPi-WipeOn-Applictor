aded58e54f11dadcc83006d1316648c3f9c4e4e8
tarasii <tarasii@gmail.com>
2014-07-30 00:16:04 +0300

    file appent fix

--- printonbutton.py
+++ printonbutton.py
@@ -13,33 +13,34 @@ GPIO.setup(17,GPIO.OUT) #led pin
 GPIO.setup(27,GPIO.IN)  #distance sensor oin
 GPIO.output(17, True)   #led ON - redy
 
+fdev = '/dev/lp0'
 
 p = False
 while True:
-    z1 = not GPIO.input(4)
-    z2 = not GPIO.input(27)
-	z = z1 or z2
-	if ( z != p ):
-	    if ( z == True ): 
-	        GPIO.output(17, False)
-                setbarcode.barcodefromconfig()
-                if os.path.exists('/dev/lp0'):
-                   shutil.copy('/home/pi/barcode.bas', '/dev/lp0')
-		   str = "print " + datetime.utcnow().strftime("%a %b %d %H:%M:%S EEST %Y ")
-                   with open('/home/pi/prnt.log','wb') as fl:
-                      fl.write(str)
+   z1 = not GPIO.input(4)
+   z2 = not GPIO.input(27)
+   z = z1 or z2
+   if ( z != p ):
+      if ( z == True ): 
+         GPIO.output(17, False)
+         setbarcode.barcodefromconfig()
+         if os.path.exists(fdev):
+            shutil.copy('/home/pi/barcode.bas', fdev)
+	    str = "print " + datetime.utcnow().strftime("%a %b %d %H:%M:%S EEST %Y ") + "\n"
+            with open('/home/pi/prnt.log','a') as fl:
+               fl.write(str)
                       
-                else:
-		   str = "error " + datetime.utcnow().strftime("%a %b %d %H:%M:%S EEST %Y ")
-                   with open('/home/pi/prnt.log','wb') as fl:
-                      fl.write(str)
+         else:
+	    str = "error " + datetime.utcnow().strftime("%a %b %d %H:%M:%S EEST %Y ") + "\n"
+            with open('/home/pi/prnt.log','a') as fl:
+               fl.write(str)
                     
-                sleep(2)
-		GPIO.output(17, True)
+         sleep(2)
+         GPIO.output(17, True)
 		
-		#sleep(1)
+	 #sleep(1)
 
-    p = z
+   p = z
 
 GPIO.output(17, False)
 GPIO.cleanup()
