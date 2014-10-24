bd1c61b4a750bd795630bf2edc97d01a24ecb517
tarasii <tarasii@gmail.com>
2014-10-24 22:33:43 +0300

    gets id from db

--- setbarcode.py
+++ setbarcode.py
@@ -2,6 +2,9 @@
 #
 import configadapter
 from datetime import datetime
+import sqlite3
+
+fdb = '/home/pi/print.db'
 
 def barcodefromconfig(scnd = False, tofile = True):
     tp = configadapter.fromfile()
@@ -13,13 +16,24 @@ def barcodefromconfig(scnd = False, tofile = True):
     cl_d = {"white": "W",
         "brown": "B"}
 
+    #get id for new barcode from db
+    conn = sqlite3.connect(fdb)
+    c = conn.cursor()
+    c.execute('select max(id) from printid')
+    r = c.fetchone()
+    if r[0]:
+       id = r[0]+1
+    else: id = 1
+    
+    conn.close()
+
     dt = datetime.now()
 
     if (scnd == True):
-        z = (dt.day, dt.month, dt.year, tp[7],  tp[8], cl_d[tp[3]], tp[1], tp[5])
+        z = (dt.day, dt.month, dt.year, tp[7],  tp[8], cl_d[tp[3]], tp[1], tp[5], id)
     
     else:    
-        z = (dt.day, dt.month, dt.year, tp[2],  tp[4], cl_d[tp[3]], tp[1], tp[5])
+        z = (dt.day, dt.month, dt.year, tp[2],  tp[4], cl_d[tp[3]], tp[1], tp[5], id)
 
     out = str % z
 
