7ef3a6d399e06d02a72d3308f9ae012c882f8773
tarasii <tarasii@gmail.com>
2014-11-29 19:50:58 +0200

    table attributes arrange complited, example to module added

--- myhtml.py
+++ myhtml.py
@@ -21,15 +21,19 @@ def CancelButton(name):
    patern = "<input type=reset value=%s>\n"
    return patern % (name, )
 
-def Input(name, val=""):
-   patern = "<input name=%s value=%s>\n"
-   return patern % (name, val)
+def Input(name, val="", disabled=False):
+   strdis = ""
+   strval = ""
+   if disabled:
+      strdis = " disabled"
+   if val:
+      strval = " value="
+   patern = "<input name=%s%s%s%s>\n"
+   return patern % (name, strdis, strval, val)
 
-def TextBox(name, val):
-   return Label(name+": ")+Input(name,val)
+def TextBox(name, val, disabled=False):
+   return Label(name+": ")+Input(name,val,disabled)
 
-def TextBox(name, val):
-   return Label(name+": ")+Input(name,val)
 
 def HyperLink(link, text=""):
    pref = "http://"
@@ -60,81 +64,102 @@ def BR():
 #<<tables   
 def TableElement(eltype, tp):
    res = ""
-   res = res + CheckTuple(tp)
-   if not attr.startswith(" "):
+   attr = ""
+
+   #print tp
+   #print eltype
+   #return ""
+   ttp = TupleToString(tp)
+   res = ttp[0]
+   attr = ttp[1]
+
+   eltype.lower()
+   if eltype.startswith("td"):
+      attr = attr.replace("cs=","colspan=")
+   elif eltype.startswith("table"):
+      attr = attr.replace("cs=","cellspacing=")
+      attr = attr.replace("br=","border=")
+      attr = attr.replace("wd=","width=")
+
+   if (not attr.startswith(" ")) and (attr):
       attr = " "+attr
  
    patern = "<%s%s>%s</%s>\n"
    return patern % (eltype, attr, res, eltype)
 
 def TableCell(tp, cs=0):
-   attr = ""
-   if cs!=0:
-      attr = " colspan = %s" % cs
- 
    return TableElement("td", tp)
 
-def TableRow(tp=""):
+def TableRow(*tp):
    return TableElement("tr", tp)
 
-def TableLine(tp=""):
-   return TableRow(tp)
+def TableLine(*tp):
+   return TableElement("tr", tp)
 
-def TableHead(tp=""):
+def TableHead(*tp):
    return TableElement("th", tp)
 
 def Table(*tp):
-   #attr = " width=100% cellspacing=1"
-   
-   #attr.lower()
-   #if attr.find("cellspacing")==-1:
-   #    attr = attr + " cellspacing=1"
-   
-   #if border:
-   #   attr = attr + " border=%s" % border
-   #
-   #if width:
-   #   attr = attr + " width=" + width
-   #for param in tp
-   #   if isinstance(param, dict):   
-
    return TableElement("table", tp)
 
 def TD(*tp):
-   #return TableCell(tp, cs)
-   return TableCell(tp)
+   return TableElement("td", tp)
 
 def TR(*tp):
-   return TableRow(tp)
+   return TableElement("tr", tp)
 
 def TH(*tp):
-   return TableHead(tp)
+   return TableElement("th", tp)
 
 #tables>>
 
-def CheckTuple(tp):
-   res = ""
-   if not tp:
-      tp = ("",)
 
-   if isinstance(tp, basestring):
-      res = res + tp
-   elif isinstance(tp, (list, tuple)):
-      for el in tp:
-         res = res + el
-   
+def DictToString(dt):
+   res = ""
+   if not dt:
+      res = ""
+   elif isinstance(dt, basestring):
+      res = dt
+   elif isinstance(dt, dict):
+      for el in dt:
+         res = res + "".join((" ", el, "=", str(dt[el])))
+
    return res
 
+def TupleToString(tp):
+   res = ""
+   attr = ""
+   if not tp:
+      res = ""
+   elif isinstance(tp, basestring):
+      res = tp
+   elif isinstance(tp, (list, tuple)):
+      if len(tp)>1:
+         res = "".join(tp[:-1])
+         attr = DictToString(tp[-1])
+      else:
+         res = "".join(tp)
+
+   return (res, attr)
+
 def Form(name, *tp):
-   res = "<form name=%s method=post>\n" % (name,)
-   res = res + CheckTuple(tp)
-   res = res + "</form>"
+   ttp = TupleToString(tp)
+   #print ttp
+   res = "<form name=%s method=post%s>\n %s </form>" % (name,ttp[1],ttp[0])
    return res
 
 def Html(name, *tp):
+   ttp = TupleToString(tp)
    res = "Content-Type: text/html\n\n"
    res = res + "<html>\n<head>\n<meta content=text/html; charset=UTF-8 />\n"
-   res = res + "<title>%s</title>\n" % (name,)
-   res = res + CheckTuple(tp)
-   res = res + "</body>\n</html>"
+   res = res + "<title>%s</title>\n<body>\n%s\n</body>\n</html>" % (name,ttp[0])
    return res
+
+if __name__ == "__main__":
+   print(Html("MyHtml module test",Form("myhtml.py", Header("My Html"),Label("Students assessment:"), 
+      Table(TR(TH("N"),TH("Name"),TH("Surname"),TH("Mark"),""),
+            TR(TD("1."),TD("John"),TD("Doe"),TH(Input("tst1")),""),
+            TR(TD("2."),TD("Michael"),TD("Cooperman","cs=2"),""),
+            TR(TD("3."),TD("Mark"),TD("Lieberman"),TH(Input("tst3")),""),
+            TR(TD("4."),TD("Alex"),TD("Miller"),TH(Input("tst4")),""),
+      "cs=1 br=1"),HyperLink("google.com"),"")))
--- myhtmltest.py
+++ myhtmltest.py
@@ -3,28 +3,22 @@
 from myhtml import *
 
 print Html("test",
-   Form("zzz.html",#CheckBox("manual", True),
-#        NL(),
-#        TextBox("test","111"),
-#        NL(),
-#        SubmitButton("save"),
-#        CancelButton("cancel"),
-#        NL(),
-#        HyperLink("google.com"),
-#        BR(),
-        Table("","xxx"
-        #   TR(TH("1"),TH("2"),TH("3")),
-        #    TR(TD("test"),TD("zzz"),TD("aaa")),
-        #    TR(TD(Input("test")),TD("MY"),TD("ny"))
+   Form("zzz.html",CheckBox("manual", True),
+        NL(),
+        TextBox("test","111"),
+        NL(),
+        SubmitButton("save"),
+        CancelButton("cancel"),
+        NL(),
+        HyperLink("google.com"),
+        BR(),
+        Table(
+           TR(TH("1"),TH("2"),TH("3"),""),
+            TR(TD("4","cs=2"),TD("5"),TD("6"),""),
+            TR(TD("7"),TD("8"),TD("9"),""),{"br":1,"cs":0,"wd":400}
            ),
         BR(),
-        Table("border = 1",
-           TR(TH("1"),TH("2"),TH("3")),
-            TR(TD("4"),TD("5"),TD("6")),
-            TR(TD("7"),TD("8"),TD("9"))
-           ),
-        BR(),
-#        Header("zzzz"),
-        Paragraph("asd asdf wer qwer qwerty"),
+        Header("zzzz"),
+        Paragraph("asd asdf wer qwer qwerty"), ""
        )
    )
